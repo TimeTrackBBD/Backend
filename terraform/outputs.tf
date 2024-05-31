cdcd91282526b1e7ebd6a3b8fee589562bd8262d
Cameron Worthington <cameron.worthington@bbd.co.za>
2024-05-31 10:31:52 +0200

    remove db stuff

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,13 +7,3 @@ output "api_public_dns" {
   description = "The public DNS address of the api server"
   value = aws_instance.dj_api[0].public_dns
 }
-
-output "database_endpoint" {
-  description = "The endpoint of the database"
-  value = aws_db_instance.dj_db.endpoint
-}
-
-output "database_port" {
-  description = "The port of the database"
-  value = aws_db_instance.dj_db.port
-}
